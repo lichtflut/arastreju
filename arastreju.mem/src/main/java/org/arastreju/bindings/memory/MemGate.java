898a58c48d382c8164248ad549a59b7992a82734
Oliver Tigges <oliver.tigges@lichtflut.de>
2012-07-06 13:41:33 +0200

    Introduced Memory binding.

---
+++ arastreju.mem/src/main/java/org/arastreju/bindings/memory/MemGate.java
@@ -0,0 +1,42 @@
+package org.arastreju.bindings.memory;
+
+import de.lichtflut.infra.exceptions.NotYetImplementedException;
+import org.arastreju.sge.ModelingConversation;
+import org.arastreju.sge.Organizer;
+import org.arastreju.sge.context.DomainIdentifier;
+import org.arastreju.sge.spi.abstracts.AbstractArastrejuGate;
+
+/**
+ * <p>
+ *  Gate for the Arastreju embedded in memory datastore.
+ * </p>
+
+ * <p>
+ * Created 06.07.12
+ * </p>
+ *
+ * @author Oliver Tigges
+ */
+public class MemGate extends AbstractArastrejuGate {
+
+    public MemGate(DomainIdentifier domainIdentifier) {
+        super(domainIdentifier);
+    }
+
+    // ----------------------------------------------------
+
+    @Override
+    public ModelingConversation startConversation() {
+        return new MemModellingConversation();
+    }
+
+    @Override
+    public Organizer getOrganizer() {
+        throw new NotYetImplementedException();
+    }
+
+    @Override
+    public void close() {
+        throw new NotYetImplementedException();
+    }
+}
---
+++ arastreju.mem/src/test/java/org/arastreju/bindings/memory/MemModelingConversationTest.java
@@ -0,0 +1,17 @@
+package org.arastreju.bindings.memory;
+
+/**
+ * <p>
+ *  Test case for MemModelingConversation.
+ * </p>
+
+ * <p>
+ * Created 06.07.12
+ * </p>
+ *
+ * @author Oliver Tigges
+ */
+public class MemModelingConversationTest {
+
+
+}
