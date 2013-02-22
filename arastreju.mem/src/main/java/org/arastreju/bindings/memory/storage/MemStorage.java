db1cd5939632eb76283f72bc8e2185439104fe06
Oliver Tigges <oliver.tigges@lichtflut.de>
2013-02-22 16:08:27 +0100

    Introduced MemAssociationResolver.

--- arastreju.mem/src/main/java/org/arastreju/bindings/memory/storage/MemStorage.java
+++ arastreju.mem/src/main/java/org/arastreju/bindings/memory/storage/MemStorage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.arastreju.bindings.memory.storage;
 
 import de.lichtflut.infra.exceptions.NotYetImplementedException;
@@ -54,7 +69,7 @@ public class MemStorage implements GraphDataStore {
 
     @Override
     public AssociationResolver createAssociationResolver(WorkingContext ctx) {
-        throw new NotYetImplementedException();
+        return new MemAssociationResolver(ctx, this);
     }
 
     @Override
@@ -65,5 +80,11 @@ public class MemStorage implements GraphDataStore {
     @Override
     public TxProvider getTxProvider() {
         return new MemTransactionProvider();
+    }
+
+    // ----------------------------------------------------
+
+    StoredResource getStoreEntry(QualifiedName qn) {
+        return store.get(qn);
     }
 }
