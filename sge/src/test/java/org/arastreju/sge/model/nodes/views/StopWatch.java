3db9a8b6d0c982c428cd7ae44a22f50fa54eb2bd
Oliver Tigges <oliver.tigges@lichtflut.de>
2013-07-13 14:10:23 +0200

    Cleanup of license information.

--- sge/src/test/java/org/arastreju/sge/model/nodes/views/StopWatch.java
+++ sge/src/test/java/org/arastreju/sge/model/nodes/views/StopWatch.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
+ * Copyright (C) 2013 lichtflut Forschungs- und Entwicklungsgesellschaft mbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
